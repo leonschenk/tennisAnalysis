7c18e76711b250b29d7b1c1b20600b866dafcc0a
Leon Schenk <leon_schenk@live.nl>
2021-06-09 21:47:08 +0000

    Changes

--- conanfile.py
+++ conanfile.py
@@ -1,20 +1,36 @@
 from conans import ConanFile
-from mesonbuild.build import Meson
+from conan.tools.meson import Meson
+
+import os
 
 class TennisAnalysisFile(ConanFile):
-    generators = ["pkg_config", "MesonToolchain"]
+    name = "TennisAnalysis"
+    version = "1.0.0"
+    generators = ["PkgConfig", "MesonToolchain"]
     requires = "opencv/4.5.2"
     settings = "os", "compiler", "arch", "build_type"
+    exports_sources = "src/*"
+
+    def init(self):
+        self.build_policy = "missing"
 
     def configure(self):
-        self.folders.generators = "generators"
-
         self.options["opencv"].with_openexr = False
         self.options["opencv"].dnn = False
 
+    def layout(self):
+        self.folders.source = ""
+        self.folders.build = "build"
+        self.folders.generators = os.path.join(self.folders.build, "generators")
+        self.folders.package = os.path.join(self.folders.build, "package")
+
     def build(self):
-        meson = Meson(self)
-        meson.configure(source_folder="src")
+        meson = Meson(self, build_folder=None)
+        meson.configure()
         meson.build()
 
+    def package(self):
+        meson = Meson(self, build_folder=None)
+        meson.set_prefix()
+        meson.install()
         
